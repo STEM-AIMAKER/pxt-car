7bb14156ff0fa8494bc615749ac364609a8a0ebf
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-01-14 13:28:09 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 //% weight=10 color=#1E90FF icon="\uf136"
 namespace COBot {
     let serialInited = 0;
+    let pinsInited = 0;
 
     export enum CarActions {
         //% blockId="GoForward" block="Go Forward"
@@ -34,16 +35,86 @@ namespace COBot {
         Black=3
     }
 
+    export enum Motors{
+        //% blockId="LeftMotor" block="Left Motor"
+        LeftMotor=0,
+        //% blockId="RightMotor" block="Right Motor"
+        RightMotor=1,
+        //% blockId="BothMotor" block="Both Motor"
+        BothMotor=2
+    }
+
+    export enum Directions
+    {
+        //% blockId="Positive" block="Positive"
+        Positive=0,
+        //% blockId="Negative" block="Negative"
+        Negative=1
+    }
+
     function initSerial(): void {
         if( 0 == serialInited ) {
             serial.redirect(
-                SerialPin.P12,
-                SerialPin.P14,
+                SerialPin.P8,
+                SerialPin.P16,
                 BaudRate.BaudRate9600
             );
             serialInited = 1;
         }
     }
+
+    function initPins(): void {
+        if (pinsInited == 0 )
+        {
+            pinsInited = 1;
+            pins.analogSetPeriod(AnalogPin.P0, 10);
+            pins.analogSetPeriod(AnalogPin.P1, 10);
+        }
+    }
+
+    function executeLeftMotor( speed: number, direct: Directions ): void {
+        if( direct == Directions.Positive )
+            pins.digitalWritePin(DigitalPin.P14, 0)
+        else
+            pins.digitalWritePin(DigitalPin.P14, 1)
+        
+        let sp = 0;
+        sp = speed * 1023 / 100;
+        pins.analogWritePin(AnalogPin.P0, sp)
+    }
+
+    function executeRightMotor( speed: number, direct: Directions ): void {
+        if (direct == Directions.Positive)
+            pins.digitalWritePin(DigitalPin.P15, 0)
+        else
+            pins.digitalWritePin(DigitalPin.P15, 1)
+
+        let sp = 0;
+        sp = speed * 1023 / 100;
+        pins.analogWritePin(AnalogPin.P1, sp)
+    }
+
+    //% blockId=switchMode block="Switch car mode to |%mode"
+    //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=1
+    //% speed.min=0 speed.max=100
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=1
+    export function runMotor(motor: Motors, speed: number, direction : Directions): void {
+        initPins();
+        switch( motor )
+        {
+            case Motors.LeftMotor:
+                executeLeftMotor(speed,direction);
+                break;
+            case Motors.RightMotor:
+                executeRightMotor(speed, direction);
+                break;
+            case Motors.BothMotor:
+                executeLeftMotor(speed, direction);
+                executeRightMotor(speed, direction);
+                break;
+        }
+    }
+
     //% weight=90
     //% blockId=switchMode block="Switch car mode to |%mode"
     //% mode.fieldEditor="gridpicker" mode.fieldOptions.columns=2
@@ -72,7 +143,6 @@ namespace COBot {
             serial.writeString(subCmd);
             basic.pause(100);
         }
-
     }
 
     //% weight=90
